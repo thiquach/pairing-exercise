6657656e16f591c084e356f6aaa090b977f00602
thi quach <thiquach@gmail.com>
2024-07-24 17:34:21 +1000

    add a calculator exercise

--- java-kotlin/src/main/java/com/tyro/pairing/Exercise.java
+++ java-kotlin/src/main/java/com/tyro/pairing/Exercise.java
@@ -1,5 +1,22 @@
 package com.tyro.pairing;
 
 public class Exercise {
+    public int add(int a, int b) {
+        return a + b;
+    }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
 }
--- java-kotlin/src/test/java/com/tyro/pairing/ExerciseTest.java
+++ java-kotlin/src/test/java/com/tyro/pairing/ExerciseTest.java
@@ -2,6 +2,7 @@ package com.tyro.pairing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExerciseTest {
@@ -11,4 +12,38 @@ public class ExerciseTest {
         assertEquals(true, false);
     }
 
+    @Test
+    void testAdd() {
+        Exercise calculator = new Exercise();
+        assertEquals(12, calculator.add(5,7));
+        assertEquals(-1, calculator.add(2,-3));
+        assertEquals(0, calculator.add(2,-2));
+    }
+
+    @Test
+    void testSubtract() {
+        Exercise calculator = new Exercise();
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+
+    @Test
+    void testMultiply() {
+        Exercise calculator = new Exercise();
+        assertEquals(6, calculator.multiply(2,3));
+        assertEquals(-6, calculator.multiply(-2,3));
+        assertEquals(0, calculator.multiply(2,0));
+    }
+
+    @Test
+    public void testDivide() {
+        Exercise calculator = new Exercise();
+        assertEquals(2.0, calculator.divide(6, 3), 0.001);
+        assertEquals(-2.0, calculator.divide(6, -3), 0.001);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.divide(6, 0);
+        });
+        assertEquals("Division by zero is not allowed.", exception.getMessage());
+    }
 }
